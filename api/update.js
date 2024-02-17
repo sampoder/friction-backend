3b04cc1f0810d0f2f9e5f8d37e77ad6fe273880e
Sam Poder <sam.r.poder@gmail.com>
2024-02-17 15:05:57 -0800

    Create update.js

---
+++ api/update.js
@@ -0,0 +1,35 @@
+import prisma from "../lib/prisma";
+import { groupStatus } from "./log";
+
+export default async function handler(req, res) {
+  let { session, image, name, email } = req.body;
+  let user = (
+    await prisma.session.findUnique({
+      where: {
+        id: session,
+      },
+      include: {
+        user: {
+          include: {
+            group: {
+              include: {
+                users: true
+              }
+            }
+          }
+        },
+      },
+    })
+  )?.user;
+  
+  if(!user || !email || !image || !name){
+    return res.json({ success: false });
+  }
+  
+  await prisma.user.update({
+    where: { id: user.id },
+    data: { email, image, name },
+  })
+
+  res.json(await groupStatus(user.group, user));
+}
