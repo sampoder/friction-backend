f0c0504c393baa107db1963f5641ae688bc94e98
Sam Poder <sam.r.poder@gmail.com>
2024-02-17 17:46:39 -0800

    ugh

--- api/log.js
+++ api/log.js
@@ -1,29 +1,38 @@
 import prisma from "../lib/prisma";
 
 function isTimeBetween(start_time_str, end_time_str, timezoneOffset) {
-    // Parse time strings
-    let startTimeParts = start_time_str.split(':');
-    let endTimeParts = end_time_str.split(':');
-    let startTime = new Date();
-    startTime.setHours(parseInt(startTimeParts[0]));
-    startTime.setMinutes(parseInt(startTimeParts[1]));
-    
-    let endTime = new Date();
-    endTime.setHours(parseInt(endTimeParts[0]));
-    endTime.setMinutes(parseInt(endTimeParts[1]));
+  // Parse time strings
+  let startTimeParts = start_time_str.split(":");
+  let endTimeParts = end_time_str.split(":");
+  let startTime = new Date();
+  startTime.setHours(parseInt(startTimeParts[0]));
+  startTime.setMinutes(parseInt(startTimeParts[1]));
 
-    // Get current time
-    let currentDate = new Date();
-    let timezoneOffsetMillis = timezoneOffset * 60 * 60 * 1000;
-    let currentTimezoneDate = new Date(currentDate.getTime() + timezoneOffsetMillis);
+  let endTime = new Date();
+  endTime.setHours(parseInt(endTimeParts[0]));
+  endTime.setMinutes(parseInt(endTimeParts[1]));
 
-    // If end time is before start time within the same day, adjust end time to the next day
-    if (endTime < startTime) {
-        endTime.setDate(endTime.getDate() + 1);
-    }
+  // Get current time
+  let currentDate = new Date();
+  let timezoneOffsetMillis = timezoneOffset * 60 * 60 * 1000;
+  let currentTimezoneDate = new Date(
+    currentDate.getTime() + timezoneOffsetMillis,
+  );
 
-    // Check if current time is not between start and end time
-    return !(currentTimezoneDate >= startTime && currentTimezoneDate <= endTime);
+  startTime.setDate(currentTimezoneDate.getDate());
+  endTime.setDate(currentTimezoneDate.getDate());
+
+  // If end time is before start time within the same day, adjust end time to the next day
+  if (endTime < startTime) {
+    endTime.setDate(endTime.getDate() + 1);
+  }
+
+  console.log(endTime);
+  console.log(startTime);
+  console.log(currentTimezoneDate);
+
+  // Check if current time is not between start and end time
+  return (currentTimezoneDate >= startTime && currentTimezoneDate <= endTime);
 }
 
 function getStartOfDayInTimezone(timezoneOffset) {
