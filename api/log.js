dfe436a1dc75f829e276bc87e563623dc46a9831
Sam Poder <sam.r.poder@gmail.com>
2024-02-18 00:16:36 -0800

    Update log.js

--- api/log.js
+++ api/log.js
@@ -85,7 +85,7 @@ export async function groupStatus(group, user){
     blame[scroll.userId] = blame[scroll.userId] ? blame[scroll.userId] + scroll.distance : scroll.distance
   })
   
-  let friction = sum - ((((new Date()).getTime() - getStartOfDayInTimezone(group.tzOffset))) * 2)
+  let friction = sum - ((((new Date()).getTime() - getStartOfDayInTimezone(group.tzOffset))) * group.users.length)
   
   if(group && group.startBreak && group.endBreak && isTimeBetween(group.startBreak, group.endBreak, group.tzOffset)){
     friction = 0
