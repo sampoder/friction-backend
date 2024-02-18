23a78ca71192882ac7acdce2011bdc951e40a1c6
Sam Poder <sam.r.poder@gmail.com>
2024-02-17 17:35:52 -0800

    Update log.js

--- api/log.js
+++ api/log.js
@@ -1,20 +1,20 @@
 import prisma from "../lib/prisma";
 
-function isTimeBetween(start_time_str, end_time_str, timezone_offset) {
+function isTimeBetween(start_time_str, end_time_str, timezoneOffset) {
     // Parse time strings
     let startTimeParts = start_time_str.split(':');
     let endTimeParts = end_time_str.split(':');
     let startTime = new Date();
-    startTime.setHours(parseInt(startTimeParts[0]) + timezone_offset);
+    startTime.setHours(parseInt(startTimeParts[0]));
     startTime.setMinutes(parseInt(startTimeParts[1]));
     
     let endTime = new Date();
-    endTime.setHours(parseInt(endTimeParts[0]) + timezone_offset);
+    endTime.setHours(parseInt(endTimeParts[0]));
     endTime.setMinutes(parseInt(endTimeParts[1]));
 
     // Get current time
-    let currentTime = new Date();
-    currentTime.setHours(currentTime.getHours() + timezone_offset);
+    let timezoneOffsetMillis = timezoneOffset * 60 * 60 * 1000;
+    let currentTimezoneDate = new Date(currentDate.getTime() + timezoneOffsetMillis);
 
     // If end time is before start time within the same day, adjust end time to the next day
     if (endTime < startTime) {
@@ -22,7 +22,7 @@ function isTimeBetween(start_time_str, end_time_str, timezone_offset) {
     }
 
     // Check if current time is not between start and end time
-    return !(currentTime >= startTime && currentTime <= endTime);
+    return !(currentTimezoneDate >= startTime && currentTimezoneDate <= endTime);
 }
 
 function getStartOfDayInTimezone(timezoneOffset) {
